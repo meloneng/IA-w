5df47fd4bae003593e93110dc8003b704733d842
meloneng <meloneng@protonmail.com>
2023-01-25 13:28:55 -0300

    states generation

--- Jogo dos 8/exe.py
+++ Jogo dos 8/exe.py
@@ -1,16 +1,17 @@
 import random
+from queue import Queue
 
 # Biblioteca para debug
 from icecream import ic
 ic.configureOutput(prefix='Debug| ')
 #ic.disable()
 
-# Inicializando o quadro
+# Inicializando o board
 def boardInit():
     values = [1, 2, 3, 4, 5, 6, 7, 8, -1]
     random.shuffle(values)
     ic(values)
-    quadro = []
+    board = []
     aux1 = 0
     for i in range(3):
         row = []
@@ -18,9 +19,32 @@ def boardInit():
             aux2 = values[aux1]
             row.append(aux2)
             aux1+=1
-        quadro.append(row)
-    ic(quadro)
-    return quadro
+        board.append(row)
+    ic(board)
+    return board
 
 
-quadro = boardInit()
+def findBlank(board):
+    for i_idx, i in enumerate(board):
+        for j_idx, j in enumerate(i):
+            if(j == -1):
+                return (i_idx,j_idx)
+
+
+
+def statesGen(board):
+    blank = findBlank(board)
+    if(blank[0] == 0):
+        if(blank[1] == 0):
+        if(blank[1] == 2)
+    if(blank[0] == 2):
+        if(blank[1] == 0)
+        if(blank[1] == 2)
+    
+
+
+
+estadoFinal = [[1, 2, 3], [4, 5, 6], [7, 8, -1]]
+board = boardInit()
+states = Queue()
+
